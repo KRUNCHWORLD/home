87bc4a13ef664b215a7ce1e2ebe59fdf04040662
KRUNCHWORLD <whycantitbemuchshorter@gmail.com>
2026-07-03 14:33:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const sounds = [
     "274445__creeper_ciller78__grunt1.wav",
     "661242__deqsterslab__swing-grunt.mp3",
     "freesound_community-vargan-00-20-74013.mp3"
+    "Scale in C Step 4.wav"
+    "Scale in C Step 3.wav"
+    "Scale in C Step 2.wav"
+    "Scale in C Step 1.wav"
 
 ];
 
